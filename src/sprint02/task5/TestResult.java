17a41a206808e6efd70950ef3a75ace1c7663c5a
Nick Underhill <m.podopryhora@gmail.com>
2020-06-18 00:16:08 +0300

    Sprint 02 - Task05

---
+++ src/sprint02/task5/TestResult.java
@@ -0,0 +1,16 @@
+package sprint02.task5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestResult {
+    public static void main(String[] args) {
+        List<Figure> figureList = new ArrayList<>();
+        figureList.add(new Square(4));
+        figureList.add(new Square(5));
+        figureList.add(new Rectang(2, 3));
+
+        double perim = new MyUtils().sumPerimeter(figureList);
+        System.out.println(perim);
+    }
+}
