667011366fcf30a1158c2e20dd0023eec48d4e4f
Nick Underhill <m.podopryhora@gmail.com>
2020-07-02 23:22:24 +0300

    Sprint07 - Task01 v1

---
+++ src/sprint07/task1/CheckCamelCase.java
@@ -0,0 +1,88 @@
+package sprint07.task1;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+import java.lang.reflect.Field;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class CheckCamelCase {
+    public static final String CAMELCASE_PATTERN = "[a-z]+((\\d)|([A-Z0-9][a-z0-9]+))*([A-Z])?";
+
+    static boolean checkAndPrint(Class clazz) {
+        boolean result = true;
+
+        List<Method> methodList = Arrays
+                .stream(clazz.getMethods())
+                .filter(method -> method.isAnnotationPresent(CamelCase.class))
+                .collect(Collectors.toList());
+
+        List<Method> wrongMethodList = new ArrayList<>();
+        for (Method method1 : methodList) {
+            if (!Pattern.matches(CAMELCASE_PATTERN, method1.getName())) {
+                wrongMethodList.add(method1);
+                result = false;
+            }
+        }
+
+        wrongMethodList.stream()
+                .map(
+                        method -> String.format(
+                                "method %s.%s doesn't satisfy camelCase naming convention",
+                                clazz.getSimpleName(),
+                                method.getName()
+                        )
+                )
+                .forEach(System.out::println);
+
+        return result;
+    }
+}
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface CamelCase {
+}
+
+
+class ClassForAnnot {
+    @CamelCase
+    public static void example() {
+    }
+
+    @CamelCase
+    public void Example() {
+    }
+
+    public static void _main(String[] args) {
+    }
+}
+
+class Class1 {
+    @CamelCase
+    public void correct() {
+    }
+
+    @CamelCase
+    public void InCorrect() {
+    }
+
+    @CamelCase
+    public void JustMethod() {
+    }
+}
+
+class Class2 {
+    @CamelCase
+    public void correct() {
+    }
+
+    @CamelCase
+    public void oneMoreCorrect() {
+    }
+}
+
---
+++ src/sprint07/task1/testResult.java
@@ -0,0 +1,9 @@
+package sprint07.task1;
+
+public class testResult {
+    public static void main(String[] args) {
+        System.out.println(CheckCamelCase.checkAndPrint(Class1.class));
+        System.out.println(CheckCamelCase.checkAndPrint(Class2.class));
+        System.out.println(CheckCamelCase.checkAndPrint(ClassForAnnot.class));
+    }
+}
