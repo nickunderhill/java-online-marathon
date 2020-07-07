9a7c9cbd576a49713a654b33e845782edd983cb9
Nick Underhill <m.podopryhora@gmail.com>
2020-07-07 18:51:14 +0300

    Sprint08 - Task02

--- src/sprint08/task1/ParallelCalculator.java
+++ src/sprint08/task1/ParallelCalculator.java
@@ -3,7 +3,8 @@ package sprint08.task1;
 import java.util.function.BinaryOperator;
 
 public class ParallelCalculator implements Runnable {
-    int operand1, operand2, result;
+    int operand1, operand2;
+    public int result;
     BinaryOperator<Integer> operator;
 
     public ParallelCalculator(BinaryOperator<Integer> operator, int operand1, int operand2) {
@@ -15,6 +16,5 @@ public class ParallelCalculator implements Runnable {
     @Override
     public void run() {
         result = operator.apply(operand1, operand2);
-        System.out.println(result);
     }
 }
